307635680b36112270506c2a060162ce9886fe63
Smyrnov Vlad <vladsimovv@gmail.com>
2025-07-05 23:24:56 +0300

    Add User entity

---
+++ src/main/java/com/example/Spring_Security/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.Spring_Security.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name= "password", nullable = false)
+    private String password;
+
+
+}
