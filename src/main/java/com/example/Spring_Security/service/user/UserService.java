b4c2e95e86e6c9b2de16e958ee7971009ffda586
Smyrnov Vlad <vladsimovv@gmail.com>
2025-07-06 17:51:29 +0300

    Add userService interface

---
+++ src/main/java/com/example/Spring_Security/service/user/UserService.java
@@ -0,0 +1,14 @@
+package com.example.Spring_Security.service.user;
+
+import com.example.Spring_Security.dto.UserDtoRequest;
+import com.example.Spring_Security.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("userService")
+public interface UserService {
+    void save(UserDtoRequest request);
+    User findById(Long id);
+    List<User> findAll();
+}
