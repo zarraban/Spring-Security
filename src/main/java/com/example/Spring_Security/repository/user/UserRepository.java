4011316973ec761aa171fcfa785d6bbfc988cf2b
Smyrnov Vlad <vladsimovv@gmail.com>
2025-07-06 17:48:01 +0300

    Add userRepository

---
+++ src/main/java/com/example/Spring_Security/repository/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.Spring_Security.repository.user;
+
+import com.example.Spring_Security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+}
