6f6db26e45b7463d29a6e12430af139e1673146e
Smyrnov Vlad <vladsimovv@gmail.com>
2025-07-06 17:47:15 +0300

    Add roleRepository

---
+++ src/main/java/com/example/Spring_Security/repository/role/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.Spring_Security.repository.role;
+
+import com.example.Spring_Security.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
