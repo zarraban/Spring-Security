6c33500faa0b2558eacd707bf5b7c91fe5605c77
Smyrnov Vlad <vladsimovv@gmail.com>
2025-07-11 12:12:23 +0300

    Change endpoint permissions

--- src/main/java/com/example/Spring_Security/configuration/AppSecurity.java
+++ src/main/java/com/example/Spring_Security/configuration/AppSecurity.java
@@ -9,42 +9,40 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.util.AntPathMatcher;
+
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurity {
 
-    private final UserDetailsService userDetailsService;
-
-    public AppSecurity(
-            UserDetailsServiceImpl userDetailsService
-    ){
-        this.userDetailsService = userDetailsService;
-    }
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Autowired
-    public void configGlobal(AuthenticationManagerBuilder auth, UserDetailsService userDetailsService) throws Exception{
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public void configGlobal(AuthenticationManagerBuilder auth) throws Exception{
         auth
                 .userDetailsService(userDetailsService)
-                .passwordEncoder(getPasswordEncoder());
+                .passwordEncoder(passwordEncoder);
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize->
-                        authorize.requestMatchers("/register/**").permitAll()
-                                .requestMatchers("/index").permitAll()
-                                .requestMatchers("/users").hasRole("ADMIN"))
+                        authorize.requestMatchers(
+                                "/register/**",
+                                "/",
+                                "/index",
+                                "login",
+                                "/register"
+                                        ).permitAll()
+                                .requestMatchers("/users").hasRole("ADMIN")
+                                .anyRequest().authenticated())
+
                 .formLogin(form->
                         form.loginPage("/login")
                                 .loginProcessingUrl("/login")
