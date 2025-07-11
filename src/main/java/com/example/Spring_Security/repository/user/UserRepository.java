fdf9636da3ea7ff031dc283e9545b82e8cdfee52
Smyrnov Vlad <vladsimovv@gmail.com>
2025-07-11 12:14:36 +0300

    Add findByEmail() to UserRepository

--- src/main/java/com/example/Spring_Security/repository/user/UserRepository.java
+++ src/main/java/com/example/Spring_Security/repository/user/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository("userRepository")
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
